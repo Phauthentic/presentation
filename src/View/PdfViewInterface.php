bffcb48da62f68fed848cc16df877ceb2731e833
Florian Krämer <florian.kraemer@kreative-design.net>
2019-01-26 19:46:13 +0100

    Start of pdf rendering

---
+++ src/View/PdfViewInterface.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types = 1);
+
+namespace Phauthentic\Presentation\View;
+
+/**
+ * Interface for data transfer objects (DTOs) to pass the data that needs to be
+ * rendered to the renderer
+ */
+interface PdfViewInterface
+{
+
+}
